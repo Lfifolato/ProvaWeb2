88d282e8f0b938a242eb8de5507dcd599b3c628f
Lfifolato <ldalri1999@gmail.com>
2020-10-05 19:08:08 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProvaWeb2.Data;
+using ProvaWeb2.Models;
+
+namespace ProvaWeb2.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Aluno
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Alunos.ToListAsync());
+        }
+
+        // GET: Aluno/Details/5
+        public async Task<ActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // GET: Aluno/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Aluno/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Nome,Endereco,Filiacao,Curso,Telefone,Email")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alunos.Add(aluno);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(aluno);
+        }
+
+        // GET: Aluno/Edit/5
+        public async Task<ActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Aluno/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Nome,Endereco,Filiacao,Curso,Telefone,Email")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aluno).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(aluno);
+        }
+
+        // GET: Aluno/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            if (aluno == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Aluno/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            Aluno aluno = await db.Alunos.FindAsync(id);
+            db.Alunos.Remove(aluno);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using ProvaWeb2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProvaWeb2.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
